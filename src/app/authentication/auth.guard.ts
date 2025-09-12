efa016161b48194d23ea5385487e4dfb601870dd
HP2 <shindesourabh081@gmail.com>
2025-09-12 13:59:41 +0530

    update auth guard

--- src/app/authentication/auth.guard.ts
+++ src/app/authentication/auth.guard.ts
@@ -28,48 +28,19 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | boolean | UrlTree {
-    // allow all authentication pages
-    console.log('Guard checking', state.url);
 
-    if (state.url.startsWith('/dashboard/main')) {
-      return true;
-    }
 
     const userData = localStorage.getItem('userData');
     const rolePermissions = localStorage.getItem('userRolePermissions');
-
-    // Not logged in → redirect to sign in
-    if (!userData || !rolePermissions) {
-      return this.router.parseUrl('/authentication/signin');
-    }
-
-    const permissions = JSON.parse(rolePermissions);
     const currentPath = state.url;
 
-    // Validate route against menus + permissions
-    return runInInjectionContext(this.injector, () =>
-      this.menuService.fetchMenus().pipe(
-        map((menus: any[]) => {
-          const matchedMenu = menus.find((m) =>
-            currentPath.includes(m.menu_url)
-          );
+    if (userData && currentPath.startsWith('/dashboard/main')) {
+      return true;
+    }
 
-          if (!matchedMenu) {
-            return this.router.parseUrl('/authentication/access-denied');
-          }
-
-          const userMenuPermission = permissions.find(
-            (p: any) => p.menuId === matchedMenu.id // or menu_name as per your data
-          );
-
-          if (userMenuPermission && userMenuPermission.permissions?.showMenu) {
-            return true;
-          }
-
-          return this.router.parseUrl('/authentication/access-denied');
-        }),
-        catchError(() => of(this.router.parseUrl('/authentication/access-denied')))
-      )
-    );
+    if (userData) {
+      return true;
+    }
+   return this.router.parseUrl('/authentication/signin')
   }
 }
