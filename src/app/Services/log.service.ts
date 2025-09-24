d17d5e481c3a366ce2963815373e4af6c8844273
HP2 <shindesourabh081@gmail.com>
2025-09-24 14:10:49 +0530

    Add LogService for activity logging with Firebase integration

---
+++ src/app/Services/log.service.ts
@@ -0,0 +1,53 @@
+import {Injectable, isDevMode} from '@angular/core';
+import {AngularFireDatabase} from "@angular/fire/compat/database";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {where} from "@angular/fire/firestore";
+import {environment} from "../../environments/environment";
+
+export interface ActivityLog {
+  date: number,
+  section: string,
+  action: string,
+  user?: string,
+  description: string
+  currentIp: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+  currentIp = localStorage.getItem('currentip')!
+  collectionName='activityLog'
+
+  constructor(
+    private readonly mDatabase: AngularFireDatabase,
+    private readonly mFirestore:AngularFirestore
+
+  ) {
+
+  }
+
+  async addLog (activity:ActivityLog)  {
+    // activity.currentIp =this.currentIp
+    let email =await localStorage.getItem('userEmail')
+    // alert(email)
+    activity.user=   email!
+    activity.description=activity.description + ' '+ email
+    activity.currentIp = this.currentIp
+    this.mDatabase.list('activityLog').push(activity)
+    console.log('Log Added ',JSON.stringify(activity))
+  }
+  getLogs(){
+    return this.mDatabase.list<ActivityLog>(this.collectionName).valueChanges()
+  }
+  getLogsByCount(i:number){
+    return this.mDatabase.list<ActivityLog>(this.collectionName,ref => ref.limitToLast(i)).snapshotChanges()
+    // return this.mDatabase.list<ActivityLog>('activityLog').snapshotChanges()
+  }
+
+
+
+
+
+}
