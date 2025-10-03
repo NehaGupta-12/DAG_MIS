1df34d102a918d64080e3fd7f093e337236c0075
AJAYofficePC <shreecomputer.ajay@gmail.com>
2025-10-03 16:58:21 +0530

    validation logic added in the outletproduct

--- src/app/module/user-list/user-list.component.ts
+++ src/app/module/user-list/user-list.component.ts
@@ -133,6 +133,11 @@ export class UserListComponent implements OnInit{
     this.router.navigate(['module/add-user']);
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
